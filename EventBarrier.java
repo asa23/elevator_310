6c7ed87d5bc0e1ea26f8348fee46dfd08b74eee1
raharris <raharris@localhost.localdomain>
2014-03-22 12:39:29 -0400

    added EventBarrier implementation

---
+++ EventBarrier.java
@@ -0,0 +1,56 @@
+
+public class EventBarrier extends AbstractEventBarrier{
+	
+	private int count;
+	private boolean signal;
+	
+	public EventBarrier() {
+		signal = false;
+		this.count = 0;
+	}
+
+	@Override
+	public synchronized void arrive() {
+		this.count += 1;
+		if (signal) {
+			return;
+		}
+		while (!signal) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public synchronized void raise() {
+		if (count == 0) {
+			signal = false;
+		}
+		while (signal) {
+			notifyAll();
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public synchronized void complete() {
+		this.count -= 1;
+		if (this.count == 0) {
+			signal = false;
+			notifyAll();
+		}
+	}
+
+	@Override
+	public synchronized int waiters() {
+		return this.count;
+	}
+
+}
